cd2c32e4f07c8577b7dca702fdb980d69c7ebee2
karol <karol.piekut@gmail.com>
2021-09-28 07:16:20 +0100

    bootstrap and jQuery setup

--- play.js
+++ play.js
@@ -1,5 +1,18 @@
 function translatePigLatin(str) {
+    let regex = /^[aieouAIEOU]/g;
+
+
+
     return str;
 }
 
 translatePigLatin("consonant");
+
+
+/*
+get each letter checked
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+ */
