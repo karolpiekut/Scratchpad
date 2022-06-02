97c0f7fa893a9b181cf2ad8e79f91a758be8deae
karolpiekut <68325867+karolpiekut@users.noreply.github.com>
2022-06-02 14:22:20 +0100

    dsktp move

--- drop it.js
+++ drop it.js
@@ -1,3 +1,9 @@
+//Drop it
+//Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+//Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+
 function dropElements(arr, func) {
     const hmm = arr.map(hm => func(hm));
     
@@ -5,4 +11,4 @@ function dropElements(arr, func) {
     return hmm;
   }
   
-  console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+  console.log(dropElements([1, 2, 3], function(n) {return n < 3; })); 
